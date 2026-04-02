3cd291256b40ca49848a30268e28dc3c9ccffaff
Alessandro <alessandrolemespaula@gmail.com>
2026-04-01 22:35:49 -0300

    01/04

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,8 @@
+import AuthRoutes from "./AuthRoutes";
+import DrawerRoutes from "./DrawerRoutes";
+
+
+export default function AppRoutes(){
+    const isAuthenticated = false;
+    return isAuthenticated ? <DrawerRoutes/> : <AuthRoutes/>;
+}

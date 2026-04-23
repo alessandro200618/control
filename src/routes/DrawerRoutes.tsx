a9003915d0479a210e23e11741df117086f87d77
alessandro200618 <alessandrolemespaula@gmail.com>
2026-04-23 01:50:11 +0000

    22/04

--- src/routes/DrawerRoutes.tsx
+++ src/routes/DrawerRoutes.tsx
@@ -1,17 +1,87 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import React from "react";
+import{
+    createDrawerNavigator,
+    DrawerContentScrollView,
+    DrawerItemList
+} from "@react-navigation/drawer";
+
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    TouchableOpacity
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "../screens/HomeScreen";
 import EntidadeScreen from "../screens/EntidadeScreen";
 import ContasPagarScreen from "../screens/ContasPagarScreen";
 import ContasReceberScreen from "../screens/ContasReceberScreen";
 
 const Drawer = createDrawerNavigator();
+function CustomDrawerContent(props: any) {
+    const user = {
+        nome: "Joao Silva",
+        foto: "https://i.pravatar.cc/100"
+    };
+    return (
+        <View style={style.container}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.header}>
+                <Image
+                source={{ uri: user.foto}}
+                style={styles.avatar}/>
+                <Text style={styles.name}>{user.nome}</Text>
+                </View>
+                <DrawerItemList {...props}/>
+            </DrawerContentScrollView>
+        <View style={styles.footer}>
+            <TouchableOpacity
+            style={styles.logouButton}
+            onPress={() => console.log("Logout")}>
+                <Ionicons name="log-out-outline" size={22} color="#fff"/>
+                <Text style={styles.logoutText}>Deslogar</Text>
+                </TouchableOpacity>
+            </View>
+            </View>
+
+    );
+}
 export default function DrawerRoutes() {
     return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={HomeScreen}  />
-            <Drawer.Screen name="Entidades" component={EntidadeScreen}  />
-            <Drawer.Screen name="Contas a Pagar" component={ContasPagarScreen}  />
-            <Drawer.Screen name="Contas a Receber" component={ContasReceberScreen} />
+        <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props}/>}
+        screenOptions={{
+            headerStyle: {
+                backgroundColor: "#1E88E5"
+            },
+            headerTintColor: "#fff",
+            drawerActiveTintColor: "#1E88E5",
+        }}>
+            <Drawer.Screen name="Home" component={HomeScreen}
+            options={{
+                drawerIcon: ({color, size}) => (
+                    <Ionicons name="home-outline" size={size} color={color}/>
+                )
+            }}/>
+            <Drawer.Screen name="Entidade" component={EntidadeScreen}
+            options={{
+                drawerIcon: ({color, size}) => (
+                    <Ionicons name="people-outline" size={size} color={color}/>
+                )
+            }}/>
+            <Drawer.Screen name="Contas a Pagar" component={ContasPagarScreen}
+            options={{
+                drawerIcon: ({color, size}) => (
+                    <Ionicons name="cash-outline" size={size} color={color}/>
+                )
+            }}/>
+            <Drawer.Screen name="Contas a Receber" component={ContasReceberScreen}
+            options={{
+                drawerIcon: ({color, size}) => (
+                    <Ionicons name="cash-outline" size={size} color={color}/>
+                )
+            }}/>
         </Drawer.Navigator>
     );
 }
